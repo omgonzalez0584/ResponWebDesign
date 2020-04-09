ca2e5863410925f284765766b83de4714cc15d38
omgonzalez0584 <om.gonzalez007@gmail.com>
2020-04-09 14:17:02 -0500

    Drag and Drop usando archivos

---
+++ drag_drop_files.js
@@ -0,0 +1,20 @@
+function iniciar(){
+  soltar = document.getElementById('cajasoltar');
+  soltar.addEventListener('dragenter', function(e) {
+          e.preventDefault(); } , false);
+  soltar.addEventListener('dragover' , function(e){
+          e.preventDefault();  } , false);
+  soltar.addEventListener('drop' , soltado, false);
+}
+
+function soltado(e){
+  e.preventDefault();
+  var archivos = e.dataTransfer.files;
+  var lista = '';
+  for(var f = 0; f < archivos.length; f++){
+    lista += 'Archivo: ' + archivos[f].name+' '+archivos[f].size+ '<br>';
+
+  }
+  soltar.innerHTML = lista;
+}
+window.addEventListener('load', iniciar, false);
